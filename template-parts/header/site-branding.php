42f1b8e061aba8c1b53abab808bbac0a81c71638
Garry Smith <ronintek0@gmail.com>
2017-07-15 12:02:15 -0400

    add custom fonts and customize navigation logo

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -11,15 +11,11 @@
 ?>
 <div class="site-branding">
 	<div class="wrap">
-
-		<?php the_custom_logo(); ?>
+		<?php if ( is_front_page() ) : ?>
+			<?php the_custom_logo(); ?>
+		<?php endif; ?>
 
 		<div class="site-branding-text">
-			<?php if ( is_front_page() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
 
 			<?php $description = get_bloginfo( 'description', 'display' );
 				if ( $description || is_customize_preview() ) : ?>
